8f678a08425f93b231dfc7545ff6c111808cdb80
Lily Shen <ls111@rice.edu>
2025-02-02 22:45:17 -0800

    Initialize full-stack structure and docker containerizaton

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Login from './components/Login';
+import PostFeed from './components/PostFeed';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+        <Login />
+        <PostFeed />
+        </div>
+    );
+}
+
+export default App;
