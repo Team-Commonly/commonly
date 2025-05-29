a4a267708d474770b7231730e295e4a83020e9c8
Sam Xu <xcjsam@g.ucla.edu>
2025-05-28 18:55:45 -0700

    Fix tests and scripts to avoid open handles

--- backend/__tests__/unit/routes/pods.test.js
+++ backend/__tests__/unit/routes/pods.test.js
@@ -25,4 +25,19 @@ describe('pods routes', () => {
     await request(app).get('/api/pods').expect(200);
     expect(controllers.getAllPods).toHaveBeenCalled();
   });
+
+  it('POST /api/pods calls createPod', async () => {
+    await request(app).post('/api/pods').send({}).expect(201);
+    expect(controllers.createPod).toHaveBeenCalled();
+  });
+
+  it('GET /api/pods/type calls getPodsByType', async () => {
+    await request(app).get('/api/pods/chat').expect(200);
+    expect(controllers.getPodsByType).toHaveBeenCalled();
+  });
+
+  it('POST /api/pods/123/join calls joinPod', async () => {
+    await request(app).post('/api/pods/123/join').expect(200);
+    expect(controllers.joinPod).toHaveBeenCalled();
+  });
 });
